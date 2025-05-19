e3d48ce7027ba9cdcbbbdd4f33256373d52b9df3
John Andre Yap <johnandresyap510@gmail.com>
2025-05-19 12:21:24 +0800

    Added Poppins font and Hero Background

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,15 +3,15 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>
+    <nav id="navbar">
       <ul className="navbar-items">
-        <li><a href="#hero">Home</a></li>
-        <li><a href="#about">About Me</a></li>
-        <li><a href="#technologies">What I Work With</a></li>
-        <li><a href="#projects">What I'm Working On</a></li>
-        <li><a href="#contact">Reach Out To Me</a></li>
+        <li class="navbar-item"><a href="#hero">Home</a></li>
+        <li class="navbar-item"><a href="#about">About Me</a></li>
+        <li class="navbar-item"><a href="#technologies">What I Work With</a></li>
+        <li class="navbar-item"><a href="#projects">What I'm Working On</a></li>
+        <li class="navbar-item"><a href="#contact">Reach Out To Me</a></li>
       </ul>
-    </div>
+    </nav>
   )
 }
 
