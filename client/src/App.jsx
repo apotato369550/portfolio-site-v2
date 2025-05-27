0fbfd2ee6bf3ab373fe3737926e2cc0e68b4b7df
John Andre Yap <johnandresyap510@gmail.com>
2025-05-27 19:35:40 +0800

    Got started with Identity Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Hero from './components/sections/Hero/Hero'
+import IdentitySection from './components/sections/IdentitySection/IdentitySection'
 
 function App() {
   return (
     <>
     <Hero />
+    <IdentitySection />
     </>
   )
 }
---
+++ client/src/components/sections/IdentitySection/IdentitySection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './IdentitySection.css';
+
+const IdentitySection = () => {
+  return (
+    <div id="identity-section">
+        <div className="identity-gradient-container">
+            <div className="identity-container">
+                <div className="statue-container">
+
+                </div>
+                <div className="pillars-container">
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default IdentitySection
