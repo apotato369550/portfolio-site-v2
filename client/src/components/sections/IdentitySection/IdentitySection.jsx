d613104d650827d4ce9ab7348f670322b7056f75
John Andre Yap <johnandresyap510@gmail.com>
2025-05-28 13:26:22 +0800

    Added statue pic

--- client/src/components/sections/IdentitySection/IdentitySection.jsx
+++ client/src/components/sections/IdentitySection/IdentitySection.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './IdentitySection.css';
+import statueImage from "../../../assets/vaporwave statue.png";
+import pillarImage from "../../../assets/vaporwave pillar.png";
 
 const IdentitySection = () => {
   return (
@@ -7,7 +9,9 @@ const IdentitySection = () => {
         <div className="identity-gradient-container">
             <div className="identity-container">
                 <div className="statue-container">
-
+                  <img className="statue" src={statueImage} alt="vaporwave statue image" />
+                  <div className="statue-ellipse-1"></div>
+                  <div className="statue-ellipse-2"></div>
                 </div>
                 <div className="pillars-container">
 
