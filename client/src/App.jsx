836e38ee4452743397bf9979b3b7f414961eea07
John Andre Yap <johnandresyap510@gmail.com>
2025-06-10 12:54:19 +0800

    Started on Location Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Hero from './components/sections/Hero/Hero'
 import IdentitySection from './components/sections/IdentitySection/IdentitySection'
+import LocationSection from './components/sections/LocationSection/LocationSection'
 
 function App() {
   return (
     <>
-    <Hero />
-    <IdentitySection />
+      <Hero />
+      <IdentitySection />
+      <LocationSection />
     </>
   )
 }
---
+++ client/src/components/sections/LocationSection/LocationSection.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './LocationSection.css';
+import treeImage from "../../../assets/vaporwave trees.png";
+import flagImage from "../../../assets/grunge_pinoy_flag.png";
+import cebuImage from "../../../assets/cebu.png";
+
+
+const LocationSection = () => {
+  return (
+    <div id="location-section">
+        <div className="location-gradient-container">
+            <div className="cebu-container">
+                <img className="cebu" src={cebuImage} alt="cebu image" />
+                <img className="flag" src={flagImage} alt="flag image" />
+            </div>
+            <div className="tree-container">
+                <img className="flag" src={treeImage} alt="flag image" />
+                <div className="title">
+                    <h1><span className="question-emphasis">Where</span> am I located?</h1>
+                </div>
+                <div className="information">
+                    <p>Currently coding life away in sunny <i>Cebu</i>, at the center of <i>Visayas, Philippines</i></p>
+                </div>
+                <div className="title">
+                    <h1><span className="question-emphasis">Where</span> did I study?</h1>
+                </div>
+                <div className="information">
+                    <p>Currently studying Computer Science at <i>The University of San Carlos</i></p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LocationSection
