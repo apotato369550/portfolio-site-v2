f2028d7b92d08449500a118385265532fa81d560
John Andre Yap <johnandresyap510@gmail.com>
2025-06-19 09:57:11 +0800

    Added background to tech stack section and changed location section gradient

--- client/src/components/sections/TechStackSection/TechStackSection.jsx
+++ client/src/components/sections/TechStackSection/TechStackSection.jsx
@@ -3,7 +3,13 @@ import './TechStackSection.css';
 
 const TechStackSection = () => {
   return (
-    <div>TechStackSection</div>
+    <div id="tech-stack-section">
+        <div className="tech-stack-image-container">
+            <div className="tech-stack-container">
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
