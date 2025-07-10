b215ac6f31e73b6c2983fa52a67b234f2376bd36
John Andre Yap <johnandresyap510@gmail.com>
2025-07-10 10:11:00 +0800

    Started work on github data fetcher

---
+++ server/fetchGithubCommits.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+const fetch = require("node-fetch");
+
+const GITHUB_USERNAME = "apotato369550";
+
+async function fetchGithubData() {
+    return;
+}
