9e37e190d606b97597755caad81f6c337d933ee3
John Andre Yap <johnandresyap510@gmail.com>
2025-09-25 23:25:02 +0800

    Added vaporwave loading screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
 import Hero from "./components/sections/Hero/Hero";
 import IdentitySection from "./components/sections/IdentitySection/IdentitySection";
 import LocationSection from "./components/sections/LocationSection/LocationSection";
@@ -9,8 +11,32 @@ import DataCampSection from "./components/sections/DataCampSection/DataCampSecti
 import ContactSection from "./components/sections/ContactSection/ContactSection";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    // Minimum loading time of 3 seconds
+    const minLoadingTime = setTimeout(() => {
+      if (isLoading) {
+        setIsLoading(false);
+        setShowContent(true);
+      }
+    }, 3000);
+
+    return () => clearTimeout(minLoadingTime);
+  }, [isLoading]);
+
+  const handleLoadingComplete = () => {
+    setIsLoading(false);
+    setShowContent(true);
+  };
+
+  if (isLoading) {
+    return <LoadingScreen onLoadingComplete={handleLoadingComplete} />;
+  }
+
   return (
-    <>
+    <div className={`app-container ${showContent ? 'fade-in' : ''}`}>
       <Hero />
       <IdentitySection />
       <LocationSection />
@@ -19,7 +45,7 @@ function App() {
       <DataCampSection />
       <ContactSection />
       <Footer />
-    </>
+    </div>
   );
 }
 
